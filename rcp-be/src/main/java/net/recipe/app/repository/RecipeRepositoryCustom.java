eb109a70f05f154e53cc1612e6c469b0fef120c1
László Körtvélyessy <laci.kortvelyessy@gmail.com>
2025-03-17 00:25:03 +0100

    added filtering

---
+++ rcp-be/src/main/java/net/recipe/app/repository/RecipeRepositoryCustom.java
@@ -0,0 +1,4 @@
+package net.recipe.app.repository;
+
+public interface RecipeRepositoryCustom {
+}
---
+++ rcp-be/src/main/java/net/recipe/app/repository/RecipeRepositoryCustomImpl.java
@@ -0,0 +1,4 @@
+package net.recipe.app.repository;
+
+public class RecipeRepositoryCustomImpl {
+}
